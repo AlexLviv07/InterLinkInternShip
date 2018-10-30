5fd0d0094a369f7d9f0b642eb824711c4ee9f56c
Oleksandr Tymoshchuk <sashatymoshchuk07@gmail.com>
2018-10-30 17:20:53 +0200

    Added implementation of classes Student and Knowledge

---
+++ Knowledge.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternShipTest
+{
+    class Knowledge
+    {
+            public enum knowledgeLevel
+            {
+                Low, BelowMedium, Medium, AboveMedium, High
+            }
+
+            public knowledgeLevel level;
+
+            public Knowledge(knowledgeLevel level)
+            {
+                this.level = level;
+            }
+        }
+    }
---
+++ Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace InternShipTest
+{
+    class Student
+    {
+        private String name;
+        private Knowledge knowledge;
+
+        public Student(String name) : this(name, new Knowledge(Knowledge.knowledgeLevel.Low))
+        {
+
+        }
+        public void setKnowledge(Knowledge knowledge)
+        {
+            this.knowledge = knowledge;
+        }
+        public Student(String name, Knowledge knowledge)
+        {
+            this.name = name;
+            this.knowledge = knowledge;
+        }
+       
+    }
+}
